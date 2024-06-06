61c3dd2f133b629fa9587fd5e0c10608c3372d86
zoorura <zoorura@gmail.com>
2024-06-06 15:34:07 +0300

    Social Site Sample

---
+++ src/Data/FollowingData.js
@@ -0,0 +1,33 @@
+import Fimg1 from "../assets/Following/img-1.jpg"
+import Fimg2 from "../assets/Following/img-2.jpg"
+import Fimg3 from "../assets/Following/img-3.jpg"
+import Fimg4 from "../assets/Following/img-4.jpg"
+import Fimg5 from "../assets/Following/img-5.jpg"
+
+export const FollowimgData =[
+    {   
+        name:"Mike Muema",
+        username:"@MikeyMike",
+        img:Fimg1
+    },
+    {  
+        name:"Violet Akinyi",
+        username:"@ViolAkinyi",
+        img:Fimg2
+    },
+    { 
+        name:"John Simon",
+        username:"@Saimo",
+        img:Fimg3
+    },
+    {   
+        name:"Linda Wetu",
+        username:"@Wangu",
+        img:Fimg4
+    },
+    {  
+        name:"Kendrick",
+        username:"@Cali",
+        img:Fimg5
+    }
+]
